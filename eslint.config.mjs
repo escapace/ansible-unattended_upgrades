6175892ba83327ab28b5de585b77aa7ccd8121f0
yyxi <178086254+yyxi@users.noreply.github.com>
2025-01-07 06:30:27 +0000

    chore: update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,14 @@ import { escapace, compose } from 'eslint-config-escapace'
 export default compose(
   escapace(),
   {
+    files: [
+      'defaults/**/*.{yml,yaml}',
+      'handlers/**/*.{yml,yaml}',
+      'meta/**/*.{yml,yaml}',
+      'molecule/**/*.{yml,yaml}',
+      'tasks/**/*.{yml,yaml}',
+      'vars/**/*.{yml,yaml}',
+    ],
     rules: {
       'yaml/sort-keys': 'off',
     },
